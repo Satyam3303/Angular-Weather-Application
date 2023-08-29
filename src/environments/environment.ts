8aee81681db54acebc0d0b18286ce98ca285caf7
SHIVAM SATYAM <78131073+Satyam3303@users.noreply.github.com>
2023-08-29 20:36:49 +0530

    1st commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment ={
+    production: false,
+    WeatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue:'weatherapi-com.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '823840d274msh717dcf6bed2db72p1eca6cjsn70ce99775a0b',
+};
